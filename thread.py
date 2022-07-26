e5bd7303050cc2e8d8f756584b6a5406f5d2723d
wzh <zhenpeng.wang@iluvatar.ai>
2022-07-26 11:45:43 +0800

    threading

--- thread.py
+++ thread.py
@@ -1,3 +1,4 @@
+# // 线程基础
 # import _thread
 # import time
 # # 为线程定义一个函数
@@ -17,33 +18,122 @@
 #     pass
 
 
-# // 使用 threading 模块创建线程
+# # // 使用 threading 模块创建线程
+# import threading
+# import time
+# exitFlag = 0
+# class myThread(threading.Thread):
+#     def __init__(self, threadID, name, delay):
+#         threading.Thread.__init__(self)
+#         self.threadID = threadID
+#         self.name = name
+#         self.delay = delay
+#     def run(self):
+#         print("开始线程：" + self.name)
+#         print_time(self.name, self.delay, 5)
+#         print("退出线程：" + self.name)
+# def print_time(threadName, delay, counter):
+#     while counter:
+#         if exitFlag:
+#             threadName.exit()
+#         time.sleep(delay)
+#         print("%s: %s" % (threadName, time.ctime(time.time())))
+#         counter -= 1
+# # 创建新线程
+# thread1 = myThread(1, "Thread-1", 1)
+# thread2 = myThread(2, "Thread-2", 2)
+# # 开启新线程
+# thread1.start()
+# thread2.start()
+# thread1.join()
+# thread2.join()
+# print("退出主线程")
+
+
+# ## // 线程同步
+# import threading
+# import time
+# class myThread(threading.Thread):
+#     def __init__(self, threadID, name, delay):
+#         threading.Thread.__init__(self)
+#         self.threadID = threadID
+#         self.name = name
+#         self.delay = delay
+#     def run(self):
+#         print("开启线程：" + self.name)
+#         threadLock.acquire() # 获取锁，用于线程同步
+#         print_time(self.name, self.delay, 3)
+#         threadLock.release() # 释放锁，开启下一个线程
+# def print_time(threadName, delay, counter):
+#     while counter:
+#         time.sleep(delay)
+#         print("%s: %s" % (threadName, time.ctime(time.time())))
+#         counter -= 1
+# threadLock = threading.Lock()
+# threads = []
+# # 创建新线程
+# thread1 = myThread(1, "Thread-1", 1)
+# thread2 = myThread(2, "Thread-2", 2)
+# # 开启新线程
+# thread1.start()
+# thread2.start()
+# # 添加线程到线程列表
+# threads.append(thread1)
+# threads.append(thread2)
+# # 等待所有线程完成
+# for t in threads:
+#     t.join()
+# print("退出主线程")
+
+
+## // 线程优先级队列
+import queue
 import threading
 import time
 exitFlag = 0
 class myThread(threading.Thread):
-    def __init__(self, threadID, name, delay):
+    def __init__(self, threadID, name, q):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
-        self.delay = delay
+        self.q = q
     def run(self):
-        print("开始线程：" + self.name)
-        print_time(self.name, self.delay, 5)
+        print("开启线程：" + self.name)
+        process_data(self.name, self.q)
         print("退出线程：" + self.name)
-def print_time(threadName, delay, counter):
-    while counter:
-        if exitFlag:
-            threadName.exit()
-        time.sleep(delay)
-        print("%s: %s" % (threadName, time.ctime(time.time())))
-        counter -= 1
+def process_data(threadName, q):
+    while not exitFlag:
+        queueLock.acquire()
+        if not workQueue.empty():
+            data = q.get()
+            queueLock.release()
+            print("%s processing %s" % (threadName, data))
+        else:
+            queueLock.release()
+        time.sleep(1)
+threadList = ["Thread-1", "Thread-2", "Thread-3"]
+nameList = ["one", "Two", "Three", "Four", "Five"]
+queueLock = threading.Lock()
+workQueue = queue.Queue(10)
+threads = []
+threadID = 1
 # 创建新线程
-thread1 = myThread(1, "Thread-1", 1)
-thread2 = myThread(2, "Thread-2", 2)
-# 开启新线程
-thread1.start()
-thread2.start()
-thread1.join()
-thread2.join()
+for tName in threadList:
+    thread = myThread(threadID, tName, workQueue)
+    thread.start()
+    threads.append(thread)
+    threadID += 1
+# 填充队列
+queueLock.acquire()
+for word in nameList:
+    workQueue.put(word)
+queueLock.release()
+# 等待队列清空
+while not workQueue.empty():
+    pass
+# 通知线程是时候退出
+exitFlag = 1
+# 等待所有线程完成
+for t in threads:
+    t.join()
 print("退出主线程")
