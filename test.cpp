84802de4ee2884dc06db62b181e1add5bcd372ef
st-lr-c <109725151+st-lr-c@users.noreply.github.com>
2022-12-09 00:46:58 +0800

    Create test.cpp

--- test.cpp
+++ test.cpp
@@ -59,7 +59,7 @@ int main()
     cout << "Crunch, crunch, Now I have " << carrots << " carrots." << endl;
     return 0;
 }
-*/
+
 // getinfo.cpp
 #include <iostream>
 int main()
@@ -72,5 +72,43 @@ int main()
     carrots = carrots + 2;      // the next line concatenates output
     cout << "Now you have " << carrots << " carrots." << endl;
     return 0;
+}
+
+// sqrt.cpp
+#include <iostream>
+#include <cmath>  // or math.h
+int main()
+{
+    using namespace std;
+    double area;
+    cout << "Enter the floor area, in square feet, of your home: ";
+    cin >> area;
+    double side;
+    side = sqrt(area);
+    cout << "That's the equivalent of a square " << side
+    << " feet to the side." << endl;
+    cout << "How fascinating! " << endl;
+    return 0;
+}
+*/
+// ourfunc.cpp 
+#include <iostream>
+void simon(int);  // function prototypr for simon()
+int main()
+{
+    using namespace std;
+    simon(3);   // call the simon() function
+    cout << "Pick an integer: ";
+    int count;
+    cin >> count;
+    simon(count);  // call it again
+    cout << "Done!" << endl;
+    return 0;
+}
+void simon(int n)   // define the simon() function
+{
+    using namespace std;
+    cout << "Simon says touch your toes " << n << " times." << endl;
+    // void functions don't need return statements
 }
 
