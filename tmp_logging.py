40fc9e7e761497c29b566459d7e47dcfd5160cc1
stlrcp <109725151+stlrcp@users.noreply.github.com>
2024-01-17 19:19:19 +0800

    Update tmp_logging.py
    update

--- tmp_logging.py
+++ tmp_logging.py
@@ -52,3 +52,49 @@ mike = Player('')
 mike.hi()
 
 tom.hi()
+
+
+
+
+
+
+
+
+#### https://blog.csdn.net/ithomer/article/details/16985379
+
+# import logging
+# import logging.handlers
+
+# LOG_FILE = 'tst.log'
+# handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1024*1024, backupCount=5)
+
+# fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(levelname)s - %(message)s'
+# formatter = logging.Formatter(fmt)
+# handler.setFormatter(formatter)
+
+# logger = logging.getLogger('tst')
+# logger.addHandler(handler)
+# logger.setLevel(logging.DEBUG)
+
+# logger.info('info msg')
+# logger.debug('debug msg')
+
+
+import logging
+import logging.handlers
+def main():
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    handler = logging.handlers.TimedRotatingFileHandler('tst.log', 'D')
+    fmt = logging.Formatter("%(asctime)s - %(pathname)s - %(filename)s - %(funcName)s - %(lineno)s - %(levelname)s - %(message)s")
+    handler.setFormatter(fmt)
+    logger.addHandler(handler)
+    logger.debug("debug msg")
+    logger.info("info msg")
+    logger.warn("warn msg")
+    logger.warning("warning msg")
+    logger.error("error msg")
+    logger.fatal("fatal msg")
+    logger.critical("critical msg")
+if __name__ == "__main__":
+    main()
